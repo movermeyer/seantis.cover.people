bf2f2cd60dc540ff859dbf37b1827bee9deefb0c
Denis Krienbühl <denis@href.ch>
2013-11-14 16:01:26 +0100

    Initial commit

---
+++ seantis/cover/people/setuphandler.py
@@ -0,0 +1,15 @@
+from seantis.plonetools import setuphandlers
+
+indexes = []
+
+
+def add_catalog_indexes(context, logger=None):
+    setuphandlers.add_catalog_indexes(
+        'seantis.cover.people', indexes, context, logger
+    )
+
+
+def import_indexes(context):
+    setuphandlers.import_indexes(
+        'seantis.cover.people', indexes, context
+    )
